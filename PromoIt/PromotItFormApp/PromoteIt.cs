2717905deffc66e76e48808adce4c893c8bc1747
w3arthur <78190695+w3arthur@users.noreply.github.com>
2022-01-20 01:12:19 +0200

    Internal functions update

--- PromoIt/PromotItFormApp/PromoteIt.cs
+++ PromoIt/PromotItFormApp/PromoteIt.cs
@@ -42,57 +42,6 @@ namespace PromotItFormApp
             return ColorTranslator.FromHtml(color); 
         }
 
-        private void ActivateButton(object btnSender)
-        {
-            if (btnSender != null)
-            {
-                if (currentButton != (Button)btnSender)
-                {
-                    DisableButton();
-                    Color color = SelectThemeColor();
-                    currentButton = (Button)btnSender;
-                    currentButton.BackColor = color;
-                    currentButton.ForeColor = Color.White;
-                    currentButton.Font = new System.Drawing.Font("Segoe UI", 11.4F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-                    panelTitleBar.BackColor = color;
-                    panelLogo.BackColor = ThemeColor.ChangeColorBrightness(color, -0.3);
-                    ThemeColor.PrimaryColor = color;    
-                    ThemeColor.SecondaryColor = ThemeColor.ChangeColorBrightness(color, -0.3);
-                    buttonCloseChildForm.Visible = true;
-
-                }
-            }
-        } 
-        private void DisableButton()
-        {
-            foreach (Control previousButton in panelMenu.Controls)
-            {
-                if (previousButton.GetType() == typeof(Button))
-                {
-                    previousButton.BackColor = Color.FromArgb(49, 54, 56);
-                    previousButton.ForeColor = Color.White;
-                    previousButton.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-                }
-            }
-        }
-
-        private void OpenChildForm(Form childForm, object btnSender)
-        {
-            if (activeForm != null)
-            {
-                activeForm.Close();
-            }
-            ActivateButton(btnSender);
-            activeForm = childForm;
-            activeForm.TopLevel = false;
-            childForm.FormBorderStyle = FormBorderStyle.None;
-            childForm.Dock = DockStyle.Fill;
-            this.panelDesktopPanel.Controls.Add(childForm);
-            this.panelDesktopPanel.Tag = childForm;
-            childForm.Show();
-            labelTitle.Text = childForm.Text;
-        }
-
         private void buttonHome_Click(object sender, EventArgs e)
         {            
             ActivateButton(sender);
@@ -115,7 +64,6 @@ namespace PromotItFormApp
 
         private void buttonRegister_Click(object sender, EventArgs e)
         {
-            
             ActivateButton(sender);
             RoleRegister.Main registerForm = new RoleRegister.Main();
             registerForm.ShowDialog();   
@@ -128,33 +76,81 @@ namespace PromotItFormApp
             loginForm.ShowDialog();
         }
 
-        private void buttonCloseChildForm_Click(object sender, EventArgs e)
+        private void buttonCloseChildForm_Click(object sender, EventArgs e) 
+        {
+            Reset();
+        }
+
+        private void panelDesktopPanel_Paint_1(object sender, PaintEventArgs e) { }
+
+
+
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = SelectThemeColor();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Segoe UI", 11.4F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+                    panelTitleBar.BackColor = color;
+                    panelLogo.BackColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    ThemeColor.PrimaryColor = color;
+                    ThemeColor.SecondaryColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    buttonCloseChildForm.Visible = true;
+
+                }
+            }
+        }
+
+        private void OpenChildForm(Form childForm, object btnSender)
         {
             if (activeForm != null)
             {
                 activeForm.Close();
-                Reset();
             }
+            ActivateButton(btnSender);
+            activeForm = childForm;
+            activeForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelDesktopPanel.Controls.Add(childForm);
+            this.panelDesktopPanel.Tag = childForm;
+            childForm.Show();
+            labelTitle.Text = childForm.Text;
         }
 
         private void Reset()
         {
+            if (activeForm == null) return;
+            activeForm.Close();
+            Reset();
+
             DisableButton();
             labelTitle.Text = "HOME";
             panelTitleBar.BackColor = Color.FromArgb(141, 145, 139);
             panelLogo.BackColor = Color.FromArgb(36, 35, 49);
             currentButton = null;
-            buttonCloseChildForm.Visible = false;   
+            buttonCloseChildForm.Visible = false;
         }
 
-        private void panelDesktopPanel_Paint(object sender, PaintEventArgs e)
+        private void DisableButton()
         {
-
+            foreach (Control previousButton in panelMenu.Controls)
+            {
+                if (previousButton.GetType() == typeof(Button))
+                {
+                    previousButton.BackColor = Color.FromArgb(49, 54, 56);
+                    previousButton.ForeColor = Color.White;
+                    previousButton.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+                }
+            }
         }
 
-        private void panelDesktopPanel_Paint_1(object sender, PaintEventArgs e)
-        {
 
-        }
     }
 }
