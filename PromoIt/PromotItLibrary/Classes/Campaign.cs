741ce33d9ba26eae0da89732a45f4bda8db785bd
Yaron Malul <yaronma@nitc.co.il>
2022-01-20 16:00:01 +0200

    Update and a delete queries were added into the database table.

--- PromoIt/PromotItLibrary/Classes/Campaign.cs
+++ PromoIt/PromotItLibrary/Classes/Campaign.cs
@@ -5,10 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PromotItLibrary.Classes
 {
     public class Campaign
-    {
+    {        
         public string Name { get; set; }
         public string Hashtag { get; set; }
         public string Url { get; set; }
@@ -19,8 +20,32 @@ namespace PromotItLibrary.Classes
             mySQL.SetParameter("_name", Name);
             mySQL.SetParameter("_hashtag", Hashtag);
             mySQL.SetParameter("_webpage", Url);
-            mySQL.SetParameter("_non_profit", Configuration.LoginUser.UserName);
+            mySQL.SetParameter("_non_profit_user_name", Configuration.LoginUser.UserName);
             return mySQL.ProceduteExecute();
         }
+
+        public bool UpdateCampaign(MySQL mySQL)
+        {
+            mySQL.Procedure("update_campaign");
+            mySQL.SetParameter("_name", Name);
+            mySQL.SetParameter("_hashtag", Hashtag);
+            mySQL.SetParameter("_webpage", Url);            
+            return mySQL.ProceduteExecute();
+        }
+
+        public bool DeleteCampaign(MySQL mySQL)
+        {
+            mySQL.Procedure("delete_campaign");            
+            mySQL.SetParameter("_hashtag", Hashtag);            
+            return mySQL.ProceduteExecute();
+        }
+        
+        public bool DisplayAndSearch(MySQL mySQL)
+        {
+            mySQL.Procedure("npo_display_search");            
+            mySQL.SetParameter("_non_profit_user_name", Configuration.LoginUser.UserName);
+            return mySQL.ProceduteExecute();
+        }
+
     }
 }
